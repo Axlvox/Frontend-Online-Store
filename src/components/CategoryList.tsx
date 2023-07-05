ee2d663e3ad49ac9bbce44207112377b627ab6ca
Matheus Raiser <matheus.raiser1@hotmail.com>
2023-07-05 17:28:38 -0400

    feat: requeste 04

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from 'react';
+import { getCategories } from '../services/api';
+
+type Categories = {
+  id: string;
+  name: string;
+};
+
 function CategoryList() {
+  const [categories, setCategories] = useState<Categories[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const data = await getCategories();
+      setCategories(data);
+    };
+
+    fetchCategories();
+  }, []);
+
   return (
-    <p>oi</p>
+    <aside>
+      <h3>Categorias:</h3>
+
+      {categories.length !== 0 && (
+        categories.map((category) => (
+          <label key={ category.id }>
+            {category.name}
+            <input data-testid="category" type="radio" name="category-buttons" id="" />
+          </label>
+        ))
+      )}
+    </aside>
   );
 }
 
